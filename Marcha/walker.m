b8e3f6f7ff06bfbee4e4086e552e7dfa53be453f
Rodrigo Garcia <rodrigogarciahoyos@gmail.com>
2018-04-18 19:08:34 -0500

    ini

--- Marcha/walker.m
+++ Marcha/walker.m
@@ -1,87 +1,61 @@
 function walker
-% WALKER  Human gait. Version: March 5, 2016.
-% This model, developed by Nikolaus Troje, is a five-term Fourier series
-% with vector-valued coefficients that are the principal components for
-% data obtained in motion capture experiments involving subjects wearing
-% reflective markers walking on a treadmill.  The components, which are
-% also known as "postures" or "eigenwalkers", correspond to the static
-% position, forward motion, sideways sway, and two hopping/bouncing
-% movements that differ in the phase relationship between the upper and
-% lower portions of the body.  The postures are also classified by gender.
-% Sliders allow you to vary the amount that each component contributes to
-% the overall motion.  A slider setting greater than 1.0 overemphasizes
-% the characteristic.  Can you see whether positive values of the gender
-% coefficient correspond to male or female subjects?
-%
-% References:
-% http://blogs.mathworks.com/cleve/2016/04/11/...
-% ...the-eigenwalker-model-of-the-human-gait
-% http://jov.arvojournals.org/article.aspx?articleid=2192503
-% http://www.biomotionlab.ca/walking.php
-% http://www.biomotionlab.ca/Text/WDP2002_Troje.pdf
-% http://www.biomotionlab.ca/Text/trojeChapter07b.pdf
+  clf
+  shg
+  
+  set(gcf,'color','white','name','Software de Marcha','menu','none', 'numbertitle','off')
+  
+  F = [];
+  M = [];
+  H = [];
+  
+  load walkers  % Cargar datos M y F
+  
+  V = (F+M)/2;  % Coeficiente matrix.
+  
+  X = reshape(V(:,1),15,3);
+  
+  L = {[1 5],[5 12],[2 3 4 5 6 7 8],[9 10 11 12 13 14 15]};
+  
+  p = zeros(1,4);
+  
+  for k = 1:4
+    p(k) = line(X(L{k},1),X(L{k},2),X(L{k},3), ...
+        'marker','o', ...
+        'markersize',10, ...
+        'linestyle','-');
+  end
+  
+  set(p(1),'tag','happy','userdata',zeros(1,3));
+  
+  axis([-750 750 -750 750 0 1500])
+  
+  lift = get(gca,'position')+[0 .04 0 0];
+  
+  set(gca,'xtick',[],'ytick',[],'ztick',[],'position',lift,'clipping','off')
+  view(160,10)
 
-% Copyright 2014-2016 Cleve Moler
-% Copyright 2014-2016 The MathWorks, Inc.
-
-% The body is represented by 15 points in three space, i.e. a vector of
-% length 45.  The data consists of F, five vectors describing the average
-% female and M, five vectors describing the average male.  Four linked
-% segments, indexed by L, are the head, torso, arms, and legs.
-
-    % Initial view
-
-    clf
-    shg
-    set(gcf,'color','white', ...
-        'name','Eigenwalker', ...
-        'menu','none', ...
-        'numbertitle','off')
-    F = [];
-    M = [];
-    H = [];
-    load walkers  % Load F and M, Females and Males -- and a surprise.
-    V = (F+M)/2;  % Coefficient matrix.
-    X = reshape(V(:,1),15,3);
-    L = {[1 5],[5 12],[2 3 4 5 6 7 8],[9 10 11 12 13 14 15]};  % Links
-    p = zeros(1,4);
-    for k = 1:4
-       p(k) = line(X(L{k},1),X(L{k},2),X(L{k},3), ...
-          'marker','o', ...
-          'markersize',10, ...
-          'linestyle','-');
-    end
-    set(p(1),'tag','happy', ...
-        'userdata',zeros(1,3));
-    axis([-750 750 -750 750 0 1500])
-    lift = get(gca,'position')+[0 .04 0 0];
-    set(gca,'xtick',[], ...
-        'ytick',[], ...
-        'ztick',[], ...
-        'position',lift, ...
-        'clipping','off')
-    view(160,10)
-
-    % Sliders and controls
-
-    labels = {'speed','stride','sway','hop','bounce','gender'};
-    sliders = zeros(1,6);
-    for j = 1:6
-       switch j
-          case 1
-              smin = 0;
-              start = 1; 
-              smax = 3;
-          case 6
-              smin = -3;
-              start = 0;
-              smax = 3;
-           otherwise
-              smin = -2;
-              start = 1;
-              smax = 2;
-       end
-       slider_txt = uicontrol('style','text', ...   % above a slider
+  % Controles deslizantes
+  labels = {'Velocidad','Direccion','Influencia','Salto','Rebotar','Genero'};
+  
+  sliders = zeros(1,6);
+  
+  for j = 1:6
+    switch j
+      case 1
+        smin = 0;
+        start = 1; 
+        smax = 3;
+      case 6
+        smin = -3;
+        start = 0;
+        smax = 3;
+      otherwise
+        smin = -2;
+        start = 1;
+        smax = 2;
+  end
+  
+  slider_txt = uicontrol('style','text', ...
            'string',sprintf('%4.2f',start), ...
            'back','white', ...
            'units','normalized', ...
@@ -96,7 +70,7 @@ function walker
            'sliderstep',[1 2]/(10*smax), ...
            'userdata',slider_txt, ...
            'callback',@slider_cb);
-       uicontrol('style','text', ...  % below a slider
+       uicontrol('style','text', ...
            'string',labels{j}, ...
            'background','white', ...
            'units','normalized', ...
@@ -107,7 +81,7 @@ function walker
         'position',[.91 .94 .08 .05], ...
         'background','white', ...
         'fontweight','bold', ...
-        'string','stop');
+        'string','Detener');
     uicontrol('style','radio', ...
         'units','normalized', ...
         'position',[.03 .90 .08 .08], ...
@@ -120,14 +94,14 @@ function walker
         'background','white', ...
         'fontangle','italic', ...
         'fontsize',get(0,'defaultuicontrolfontsize')-2, ...
-        'string',{'Click on the figure','to rotate the view'})
+        'string',{'Click en la figura','para rotar caminante'})
     cameratoolbar setmode orbit
 
-    % Start walkin'...
+    % Comenzar caminata'...
 
     period = 151.5751;
     omega = 2*pi/period;
-    fps = 120;    % 120 fps when s(1)=1
+    fps = 120;    % 120 fps
     t = 0;
     dt = 2*pi/omega/fps;
     while get(stop,'value') == 0
@@ -149,27 +123,21 @@ function walker
                 'zdata',X(L{k},3));
         end
         pause(1/(s(1)*fps))
-    end;
+    end
     cameratoolbar close
     set(stop,'value',0, ...
         'string','close', ...
         'fontweight','bold', ...
         'callback','close(gcf)')
     
- % -------------------------------------------------------------
- 
     function slider_cb(s,~)
-        % Update the text above a slider
         stxt = get(s,'userdata');
         sval = get(s,'value');
         sprf = sprintf('%4.2f',sval);
         set(stxt,'string',sprf);
-    end % slider_cb
-    
- % -------------------------------------------------------------
- 
+    end
+
     function radio_cb(s,~)
-        % Happy face
         p1 = findobj('tag','happy');
         if get(s,'value')
             set(p1,'marker','none', ...
@@ -177,7 +145,7 @@ function walker
         else
             set(p1,'marker','o', ...
                 'userdata',zeros(1,3))
-        end;
-    end % radio_cb
+        end
+    end
         
-end % walker_2016
+end
